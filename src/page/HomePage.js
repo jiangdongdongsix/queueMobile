5d9b64f04f4f9b4eae06e7b23d511859c987f68b
LongSheLA <475992348@qq.com>
2017-11-23 17:21:01 +0800

    "配置路由器"

--- src/page/HomePage.js
+++ src/page/HomePage.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import { Image,View,StyleSheet,Text } from 'react-native';
-import { Button,WingBlank, WhiteSpace } from 'antd-mobile';
+import React, {Component} from 'react';
+import {Image, View, StyleSheet, Text, FlatList} from 'react-native';
+import {Button, WingBlank, WhiteSpace} from 'antd-mobile';
 import HeaderSearchBar from '../components/HeaderSearchBar';
 import px2dp from '../utils/px2pd';
 import {Dimensions} from 'react-native';
@@ -11,15 +11,24 @@ const HomeIcon = require('../images/home_icon_normal.png');
 const HomeIconChecked = require('../images/01_home_icon_check.png')
 const shop = require('../images/pic1.png');
 export default class MyHomeScreen extends React.Component {
-    static navigationOptions ={
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: []
+        }
+    }
+
+
+    static navigationOptions = {
 
         // StackNavigator 属性部分
 
         // title:'Test1', 同步设置导航和tabbar文字,不推荐使用
         // headerTitle:'首页', // 只会设置导航栏文字,
-        header:(() => {
+        header: (() => {
             return (
-                <HeaderSearchBar />
+                <HeaderSearchBar/>
             )
         }), // 可以自定义导航条内容，如果需要隐藏可以设置为null
         // header:null,
@@ -27,118 +36,155 @@ export default class MyHomeScreen extends React.Component {
         // headerTruncatedBackTitle:'', // 设置当上个页面标题不符合返回箭头后的文字时，默认改成"返回"。
         // headerRight:{}, // 设置导航条右侧。可以是按钮或者其他。
         // headerLeft:{}, // 设置导航条左侧。可以是按钮或者其他。
-      // 设置导航条的样式。如果想去掉安卓导航条底部阴影可以添加elevation: 0,iOS去掉阴影是。
+        // 设置导航条的样式。如果想去掉安卓导航条底部阴影可以添加elevation: 0,iOS去掉阴影是。
         // 设置导航条文字样式。安卓上如果要设置文字居中，只要添加alignSelf:'center'就可以了
         // headerBackTitleStyle:{}, // 设置导航条返回文字样式。
         // headerTintColor:'green', // 设置导航栏文字颜色。总感觉和上面重叠了。
-        gesturesEnabled:true, // 是否支持滑动返回收拾，iOS默认支持，安卓默认关闭
+        gesturesEnabled: true, // 是否支持滑动返回收拾，iOS默认支持，安卓默认关闭
         // TabNavigator 属性部分
-        tabBarVisible:true, // 是否隐藏标签栏。默认不隐藏(true)
-        tabBarIcon: (({tintColor,focused}) => {
-            return(
+        tabBarVisible: true, // 是否隐藏标签栏。默认不隐藏(true)
+        tabBarIcon: (({tintColor, focused}) => {
+            return (
                 <Image
                     source={!focused ? HomeIcon : HomeIconChecked}
-                    style={[{height:25,width:25 }, {tintColor: tintColor}]}
+                    style={[{height: 25, width: 25}, {tintColor: tintColor}]}
                 />
             )
         }), // 设置标签栏的图标。需要单独设置。
-        tabBarLabel:'首页', // 设置标签栏的title。推荐这个方式。
+        tabBarLabel: '首页', // 设置标签栏的title。推荐这个方式。
     }
 
-    goQueue = () =>{
+    goQueue = () => {
         this.props.navigation.navigate('queue');
     }
 
+    componentWillMount() {
+        console.log("wojinlaile ");
+        const that = this;
+        fetch("http://172.21.84.161:3334/iqescloud/app/homePage")
+            .then((response) => response.json())
+            .then((responseJson) => {
+                console.log(responseJson);
+                if (responseJson.ErrorCode === '0') {
+                    that.setState({
+                        data: responseJson.restaurantList
+                    })
+                }
+
+            }).catch((error) => {
+            console.error(error);
+            console.error("出错了");
+        });
+    }
+
+
+    renderqueinfo(item){
+        console.log("66666");
+        console.log(item);
+        return (
+            <View style={styles.queueItem}>
+                <Text>小桌{item.tableType.eatMinNumber}-{item.tableType.eatMaxNumber}</Text>
+                <WingBlank/>
+                <Text>正在排队{item.waitPopulation}桌</Text>
+                <WingBlank/>
+                <Text>约{item.waitTime}分钟</Text>
+            </View>
+
+        )
+    }
+
+
+    renderItem(item) {
+        console.log(item)
+        return (
+            <View style={styles.shopContent}>
+                <View style={styles.shop}>
+                    <View style={styles.shopName}>
+                        <WingBlank><Text>{item.restaurantInfo.name}</Text></WingBlank>
+                        <View style={styles.buttonList}>
+                            <Button type="primary" size="small"
+                                    style={{backgroundColor: '#ffa500', borderColor: '#ffa500'}}>菜单预览</Button>
+                            <WingBlank>
+                                <Button type="primary" size="small"
+                                        style={{backgroundColor: '#ff4500', borderColor: '#ff4500'}}
+                                        onClick={this.goQueue}>排队取号</Button>
+                            </WingBlank>
+                        </View>
+                    </View>
+                    <View style={styles.shopQueue}>
+                        <WingBlank>
+                            <Image
+                                source={shop}
+                                style={{height: 85, width: 85}}
+                            />
+                        </WingBlank>
+
+                        <FlatList
+                                data={item.queueInfos.queueInfo}
+                                renderItem={
+                                    ({item}) => this.renderqueinfo(item)
+                                    }
+                            />
+
+                    </View>
+                    <View style={styles.address}>
+                        <WingBlank><Text style={styles.fontSmall}>地址：{item.restaurantInfo.address}</Text></WingBlank>
+                        <WingBlank><Text style={styles.fontSmall}>1.8公里</Text></WingBlank>
+                    </View>
+                </View>
+            </View>
+        )
+    }
 
     render() {
+        console.log(this.state.data);
         return (
-          <View style = {styles.shopContent}>
-              <View style={styles.shop}>
-                  <View style={styles.shopName}>
-                      <WingBlank><Text>软件园店</Text></WingBlank>
-                      <View style={styles.buttonList}>
-                          <Button  size="small" style={{backgroundColor:'#ffa500'}} >菜单预览</Button>
-                          <WingBlank>
-                          <Button  size="small" style={{backgroundColor:'#ff4500'}} onClick={this.goQueue}>排队取号</Button>
-                          </WingBlank>
-                      </View>
-                  </View>
-                  <View style={styles.shopQueue}>
-                      <WingBlank>
-                      <Image
-                          source={shop}
-                          style={{height:85,width:85 }}
-                      />
-                      </WingBlank>
-                      <View style={styles.queueInfo}>
-                          <View style={styles.queueItem}>
-                              <Text>小桌（1-4人）</Text>
-                              <Text>前方等待30桌</Text>
-                              <Text>约30分钟</Text>
-                          </View>
-                          <View style={styles.queueItem}>
-                              <Text>中桌（5-6人）</Text>
-                              <Text>前方等待5桌</Text>
-                              <Text>约20分钟</Text>
-                          </View>
-                          <View style={styles.queueItem}>
-                              <Text>大桌（6人以上）</Text>
-                              <Text>无需等待</Text>
-                              <Text>  </Text>
-                          </View>
-                      </View>
-                  </View>
-                  <View style={styles.address}>
-                      <WingBlank><Text style={styles.fontSmall}>地址：西安市软件园秦风阁5楼</Text></WingBlank>
-                      <WingBlank><Text style={styles.fontSmall}>1.8公里</Text></WingBlank>
-                  </View>
-              </View>
-          </View>
-        );
+            <View style={{flex: 1}}>
+                <FlatList
+                    data={this.state.data}
+                    renderItem={({item}) => this.renderItem(item)}
+                />
+            </View>
+        )
     }
 }
 
 const styles = StyleSheet.create({
-    shopContent : {
-        height:px2dp(150),
-        backgroundColor:'white',
-        margin:5,
+    shopContent: {
+        height: px2dp(150),
+        backgroundColor: 'white',
+        margin: 5,
     },
-    shop :{
-        flex:1,
-        flexDirection:'column',
+    shop: {
+        flex: 1,
+        flexDirection: 'column',
 
     },
-    buttonList:{
-      flexDirection:'row',
+    buttonList: {
+        flexDirection: 'row',
     },
-    queueInfo:{
-        flex:1,
-        flexDirection:'column',
+    queueItem: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
     },
-    queueItem :{
-        flex:1,
-        flexDirection:'row',
-        justifyContent:'space-around'
+    shopName: {
+        flex: 2,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
     },
-    shopName:{
-        flex:2,
-        flexDirection:'row',
-        justifyContent:'space-between',
-        alignItems:'center',
+    shopQueue: {
+        flex: 4,
+        flexDirection: 'row',
     },
-    shopQueue:{
-        flex:4,
-        flexDirection:'row',
+    address: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
     },
-    address:{
-        flex:1,
-        flexDirection:'row',
-        justifyContent:'space-between',
-        alignItems:'center',
-    },
-    fontSmall:{
-        fontSize:px2dp(10),
+    fontSmall: {
+        fontSize: px2dp(10),
     }
 
 })
--- src/page/QueueUp.js
+++ src/page/QueueUp.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View,Text,StyleSheet,Image } from 'react-native';
 import px2dp from '../utils/px2pd';
 import {Dimensions} from 'react-native';
+import { List, InputItem, WhiteSpace,Button,Switch  } from 'antd-mobile';
 import { WingBlank } from 'antd-mobile';
 const shop = require('../images/pic1.png');
 const phone = require('../images/02_call_button.png');
@@ -9,6 +10,25 @@ const loctionIcon = require('../images/02_location_pic.png');
 const timeIcon = require('../images/02_time_pic.png');
 
 export default class QueueUp extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            IsSeat: false,
+            IsReceiveMessage:true
+        };
+    }
+
+    changeSeatFlag = () =>{
+        let flag = this.state.IsSeat;
+        this.setState({
+            IsSeat: !flag,
+
+        })
+    }
+
+
+
     static navigationOptions ={
         title:'排队取号',
     };
@@ -23,10 +43,9 @@ export default class QueueUp extends React.Component {
                                style={{height:85,width:85 }}
                            />
                        </WingBlank>
-
                        <View style={styles.shopLeft}>
                            <View style={styles.title}>
-                               <Text style = {{fontSize: 20}}> 软件园店</Text>
+                               <Text style = {{fontSize: 20}}>软件园店</Text>
                                <WingBlank>
                                    <Image
                                        source={phone}
@@ -51,9 +70,69 @@ export default class QueueUp extends React.Component {
                        </View>
                    </View>
                </View>
-               <View>
-
+               <View  style={styles.eatInput}>
+                   <WhiteSpace />
+                   <WhiteSpace />
+                   <List>
+                       <InputItem
+                           placeholder="请输入用餐人数"
+                       >用餐人数</InputItem>
+                   </List>
                </View>
+               <View style={styles.IsChooseSeat}>
+                   <WingBlank>
+                     <Text style={{color:'black'}}>座位</Text>
+                   </WingBlank>
+                   <View style={styles.buttonList}>
+                       <Button  size="small" type="primary" style={{backgroundColor: this.state.IsSeat? 'grey' : '#ffa500',borderColor:this.state.IsSeat? 'grey' : '#ffa500'}} onClick={this.changeSeatFlag}>系统自动匹配座位</Button>
+                       <WingBlank>
+                           <Button  size="small" type="primary" style={{backgroundColor:this.state.IsSeat? '#ffa500' : 'grey',borderColor:this.state.IsSeat? '#ffa500' : 'grey'}} onClick={this.changeSeatFlag}>自选用餐位置</Button>
+                       </WingBlank>
+                   </View>
+               </View>
+               <View  style={{display:this.state.IsSeat ? 'flex' : 'none'}}>
+                   <List>
+                       <InputItem
+                           placeholder="请输入桌号"
+                       >桌号</InputItem>
+                   </List>
+               </View>
+               <WhiteSpace size="lg"/>
+               <View style ={styles.queueInfo}>
+                   <View style = {styles.queueContent}>
+                       <Text>餐桌类型</Text>
+                       <WhiteSpace />
+                       <Text>小桌（1-4人）</Text>
+                   </View>
+                   <View>
+                       <Text>等待桌位</Text>
+                       <WhiteSpace />
+                       <Text>4桌</Text>
+                   </View>
+                   <View>
+                       <Text>预估时间</Text>
+                       <WhiteSpace />
+                       <Text>4桌</Text>
+                   </View>
+               </View>
+               <WhiteSpace size="lg"/>
+               <View  style={styles.eatInput}>
+                   <List>
+                       <InputItem
+                           placeholder="请输入电话号码"
+                       >电话</InputItem>
+                   </List>
+               </View>
+               <WingBlank><Text style={{fontSize:px2dp(10),color:'#ffa500'}}>*建议您输入手机号码，方便我们短信通知</Text></WingBlank>
+               <WhiteSpace size="lg"/>
+               <View style={styles.IsChooseSeat}>
+                   <WingBlank>
+                       <Text style={{color:'black'}}>是否接收排队状态</Text>
+                   </WingBlank>
+                   <Switch checked={this.state.IsReceiveMessage} color="#ffa500"/>
+               </View>
+               <WhiteSpace size="xl"/>
+               <WingBlank><Button type="primary" style={{backgroundColor:'#ffa500',borderColor:'#ffa500'}}>立即取号</Button></WingBlank>
            </View>
        )
     }
@@ -86,7 +165,30 @@ const styles = StyleSheet.create({
     },
     addressTittle:{
         fontSize:10
-    }
+    },
+    eatInput:{
+        backgroundColor:'white',
+    },
+    IsChooseSeat:{
+        height:px2dp(40),
+        flexDirection:'row',
+        justifyContent:'space-between',
+        backgroundColor:'white',
+        alignItems:'center',
+    },
+    buttonList:{
+        flexDirection:'row',
+    },
+    queueInfo:{
+        height:px2dp(100),
+        flexDirection:'row',
+        justifyContent:'space-around',
+        backgroundColor:'white',
+        alignItems:'center'
+
+    },
+
+
 })
 
 
