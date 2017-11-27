056110c4c580752a43e046a743b631dc71faf0a8
lijingyanyan <yanlijing.tp@advantech.com.cn>
2017-11-27 14:00:08 +0800

    update

---
+++ src/page/Menu.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { StyleSheet,Image,View,Text,ImageBackground,Modal,Animated,TouchableOpacity,TextInput } from 'react-native';
+import { Button,Progress,List,Tabs } from 'antd-mobile';
+import px2dp from '../utils/px2pd';
+/**
+ *  菜单预览
+ */
+
