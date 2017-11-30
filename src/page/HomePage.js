59f548709a91974ffefcf07cd88e6caf8f395903
LongSheLA <475992348@qq.com>
2017-11-30 11:30:58 +0800

    "排队"

--- src/page/HomePage.js
+++ src/page/HomePage.js
@@ -107,6 +107,56 @@ export default class MyHomeScreen extends React.Component {
                     dataFlag:false
                 })
         });
+
+        storage.save({
+            key: 'loginState',   // Note: Do not use underscore("_") in key!
+            data: {
+                from: 'some other site',
+                userid: 'some userid',
+                token: 'some token'
+            },
+
+            // if not specified, the defaultExpires will be applied instead.
+            // if set to null, then it will never expire.
+            expires: 1000 * 3600
+        });
+
+        storage.load({
+            key: 'loginState',
+            // autoSync(default true) means if data not found or expired,
+            // then invoke the corresponding sync method
+            autoSync: true,
+
+            // syncInBackground(default true) means if data expired,
+            // return the outdated data first while invoke the sync method.
+            // It can be set to false to always return data provided by sync method when expired.(Of course it's slower)
+            syncInBackground: true,
+
+            // you can pass extra params to sync method
+            // see sync example below for example
+            syncParams: {
+                extraFetchOptions: {
+                    // blahblah
+                },
+                someFlag: true,
+            },
+        }).then(ret => {
+            // found data go to then()
+            console.log(ret.userid);
+        }).catch(err => {
+            // any exception including data not found
+            // goes to catch()
+            console.warn(err.message);
+            switch (err.name) {
+                case 'NotFoundError':
+                    console.log("77777");
+                    break;
+                case 'ExpiredError':
+                    // TODO
+                    console.log("77777");
+                    break;
+            }
+        })
     }
 
 
@@ -132,6 +182,7 @@ export default class MyHomeScreen extends React.Component {
                             <Button type="primary" size="small"
                                     style={{backgroundColor: '#ffa500', borderColor: '#ffa500'}}
                                     activeStyle={{backgroundColor: '#ffa500', borderColor: '#ffa500'}}
+                                    onClick = {() => this.props.navigation.navigate('menu')}
                             >菜单预览</Button>
                             <WingBlank>
                                 <Button type="primary" size="small"
